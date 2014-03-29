91562e2fb5161cb3cd2d970546fc1993e3ff60f1
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2014-03-30 00:28:10 +0400

    Add parsing command-line options

--- pa1.c
+++ pa1.c
@@ -1,7 +1,33 @@
 #include "pa1.h"
 #include <stdio.h>
+#include <unistd.h>
+
+int getopt( int argc, char * const argv[], const char * optstring );
+extern char *optarg;
+extern int opterr;
+
+const int NUMBER_OF_PROCESS = 6;
 
 int main( int argc, char * argv[] ) {
-	printf( "Hello, cruel World!\n" );
+
+	int numberOfProcess = 0;
+	int opt;
+	opterr = 0;
+
+	while ( ( opt = getopt( argc, argv, "p:" ) ) != -1 ) {
+		switch ( opt ) {
+		case 'p':
+			numberOfProcess = atoi( optarg );
+			break;
+        };
+	};
+
+	if ( numberOfProcess == 0 || numberOfProcess > 10 ) {
+		printf( "Set the default value for the number of child process: %d\n", NUMBER_OF_PROCESS );
+		numberOfProcess = NUMBER_OF_PROCESS;
+	}
+
+	printf( "Number of child process: %d\n", numberOfProcess );
+
 	return 0;
 }
