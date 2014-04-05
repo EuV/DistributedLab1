6199c422056445ae7414126cf9642926b1932e8c
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2014-04-05 14:29:59 +0400

    Add logging

--- pa1.c
+++ pa1.c
@@ -4,29 +4,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #define NUMBER_OF_PROCESS 6
+#define BUF_SIZE 100
 
 int getNumberOfProcess( int argc, char * const argv[] );
 int getopt( int argc, char * const argv[], const char * optstring );
-void ChildProcess();
-void ParentProcess();
+void ChildProcess( local_id localId );
+void ParentProcess( local_id localId );
 extern char *optarg;
 extern int opterr;
-
+int events;
 
 int main( int argc, char * argv[] ) {
 
+	events = open( evengs_log, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND );
 	int numberOfProcess = getNumberOfProcess( argc, argv );
 
-	for ( int i = 0; i < numberOfProcess; i++ ) {
+	for ( int i = 1; i <= numberOfProcess; i++ ) {
 		if ( fork() == 0 ) {
-			ChildProcess();
+			ChildProcess( i );
 			break; // Not to do fork() from the child
-		} else if ( i == numberOfProcess - 1 ) { // The last child has been created
-			ParentProcess();
+		} else if ( i == numberOfProcess ) { // The last child has been created
+			ParentProcess( 0 );
 		}
 	}
 
@@ -34,12 +39,25 @@ int main( int argc, char * argv[] ) {
 }
 
 
-void  ChildProcess() {
-	printf( "*** Child process %d is done ***\n", getpid() );
+void  ChildProcess( local_id localId ) {
+
+	pid_t PID = getpid();
+	pid_t pPID = getppid();
+	char buf[ BUF_SIZE ];
+
+	// "Process %1d (pid %5d, parent %5d) has STARTED\n";
+	sprintf( buf, log_started_fmt, localId, PID, pPID );
+	write( 1, buf, strlen( buf ) );
+	write( events, buf, strlen( buf ) );
+
+	// "Process %1d has DONE its work\n";
+	sprintf( buf, log_done_fmt, localId );
+	write( 1, buf, strlen( buf ) );
+	write( events, buf, strlen( buf ) );
 }
 
 
-void  ParentProcess() {
+void  ParentProcess( local_id localId ) {
 
 	// Waiting for all the children
 	int status;
