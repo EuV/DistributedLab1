9cfdcad186492c713bf9f32536b3df0ff9c6c594
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2014-04-06 23:53:46 +0400

    Add full IPC implementation

--- pa1.c
+++ pa1.c
@@ -13,19 +13,27 @@
 
 #define NUMBER_OF_PROCESS 2
 #define BUF_SIZE 100
+#define IPC_SUCCESS 0
+#define IPC_FAILURE -1
 
 int getNumberOfProcess( int argc, char * const argv[] );
 void ChildProcess();
 void ParentProcess();
+void makePipeLog();
 int pipes[ MAX_PROCESS_ID + 1 ][ MAX_PROCESS_ID + 1][ 2 ];
-int events;
+int eventsLog;
+int pipesLog;
 int nProc;
 local_id localId;
 
+
 int main( int argc, char * argv[] ) {
 
-	events = open( evengs_log, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND );
+	eventsLog = open( evengs_log, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND );
+	pipesLog = open( pipes_log, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND );
+
 	nProc = getNumberOfProcess( argc, argv );
+
 
 	// Create fully-connected topology with excess of duplex pipes
 	for ( int row = 0; row <= nProc; row++ ) {
@@ -34,6 +42,8 @@ int main( int argc, char * argv[] ) {
 			pipe( pipes[ row ][ col ] );
 		}
 	}
+
+	makePipeLog();
 
 	// Create a brood of process
 	for ( int i = 1; i <= nProc; i++ ) {
@@ -53,16 +63,6 @@ int main( int argc, char * argv[] ) {
 
 void ChildProcess() {
 
-	//    | 0 | i | 2 | 3
-	// -------------------
-	//  0 | - | r | - | -
-	// -------------------
-	//  i | w | - | w | w
-	// -------------------
-	//  2 | - | r | - | -
-	// -------------------
-	//  3 | - | r | - | -
-
 	// Close unused pipes
 	for ( int row = 0; row <= nProc; row++ ) {
 		for ( int col = 0; col <= nProc; col++ ) {
@@ -81,37 +81,33 @@ void ChildProcess() {
 	pid_t PID = getpid();
 	pid_t pPID = getppid();
 
+	// Create STARTED message
+	Message sendMsg;
+	sendMsg.s_header.s_magic = MESSAGE_MAGIC;
+	sendMsg.s_header.s_payload_len = strlen( sendMsg.s_payload );
+	sendMsg.s_header.s_type = STARTED;
+	sprintf( sendMsg.s_payload, log_started_fmt, localId, PID, pPID );
 
-	// STARTED
-	Message msg;
-	msg.s_header.s_magic = MESSAGE_MAGIC;
-	msg.s_header.s_payload_len = strlen( msg.s_payload );
-	msg.s_header.s_type = STARTED;
-	sprintf( msg.s_payload, log_started_fmt, localId, PID, pPID );
-	write( 1, msg.s_payload, strlen( msg.s_payload ) );
-	write( events, msg.s_payload, strlen( msg.s_payload ) );
-	send_multicast( NULL, &msg );
+	// Log STARTED message
+	write( 1, sendMsg.s_payload, strlen( sendMsg.s_payload ) );
+	write( eventsLog, sendMsg.s_payload, strlen( sendMsg.s_payload ) );
 
-
-	Message receivedMsg;
-	int startedCounter = 0;
-	while ( startedCounter != nProc - 1 ) {
-		receive_any( NULL, &receivedMsg );
-		if ( receivedMsg.s_header.s_type == STARTED ) {
-			startedCounter++;
-			printf("startedCounter = %d, needed: %d\n", startedCounter, nProc - 1 );
-		}
+	// Send STARTED message to all the neighbours
+	if ( send_multicast( NULL, &sendMsg ) == IPC_FAILURE ) {
+		printf( "Multicast error in Process %d (%d)\n", localId, PID );
+		exit( 1 );
 	}
 
 
-	// DONE
-	msg.s_header.s_magic = MESSAGE_MAGIC;
-	msg.s_header.s_payload_len = strlen( msg.s_payload );
-	msg.s_header.s_type = DONE;
-	sprintf( msg.s_payload, log_done_fmt, localId );
-	write( 1, msg.s_payload, strlen( msg.s_payload ) );
-	write( events, msg.s_payload, strlen( msg.s_payload ) );
-	send_multicast( NULL, &msg );
+	// Receive STARTED message from all the neighbours
+	Message receivedMsg;
+	int startedProcCounter = 1; // For this one has already been started
+	while ( startedProcCounter != nProc ) {
+		receive_any( NULL, &receivedMsg );
+		// TODO: if ( receivedMsg.s_header.s_type == STARTED ) { ...
+		// TODO: if ( result != 0 ) { ...
+		startedProcCounter++;
+	}
 
 
 	// Close other pipes
@@ -121,20 +117,11 @@ void ChildProcess() {
 		close( pipes[ i ][ localId ][ 0 ] );
 	}
 
+	// printf( "The last sigh of %d (%d) process\n", localId, getpid() );
 }
 
 
 void ParentProcess() {
-
-	//    | p | 1 | 2 | 3
-	// -------------------
-	//  p | - | - | - | -
-	// -------------------
-	//  1 | r | - | - | -
-	// -------------------
-	//  2 | r | - | - | -
-	// -------------------
-	//  3 | r | - | - | -
 
 	// Close unused pipes
 	for ( int row = 0; row <= nProc; row++ ) {
@@ -147,26 +134,30 @@ void ParentProcess() {
 	}
 
 
-
+	// Receive STARTED message from all the children
 	Message receivedMsg;
-	int startedCounter = 0;
-	while ( startedCounter != nProc ) {
+	int startedProcCounter = 0;
+	while ( startedProcCounter != nProc ) {
 		receive_any( NULL, &receivedMsg );
-		if ( receivedMsg.s_header.s_type == STARTED ) {
-			startedCounter++;
-			// printf("startedCounter = %d, needed: %d\n", startedCounter, nProc );
-		}
+		// TODO: if ( receivedMsg.s_header.s_type == STARTED ) { ...
+		// TODO: if ( result != 0 ) { ...
+		startedProcCounter++;
 	}
 
 	// Waiting for all the children
-	while( wait( NULL ) != -1 ) { }
+	int status;
+	pid_t pid;
+	while ( ( pid = wait( &status ) ) != -1 ) {
+		printf( "Process %d has been done with exit code %d\n", pid, status );
+		if( WIFSIGNALED( status ) ) printf( "!!! Interrupted by signal %d !!!\n", WTERMSIG( status ) );
+    }
 
 	// Close other pipes
 	for ( int row = 1; row <= nProc; row++ ) {
 		close( pipes[ row ][ 0 ][ 0 ] );
 	}
 
-	printf("*** Parent is done ***\n");
+	printf("*** Parent has been done ***\n");
 }
 
 
@@ -194,26 +185,67 @@ int getNumberOfProcess( int argc, char * const argv[] ) {
 }
 
 
-int send_multicast( void * self, const Message * msg ) {
-	for ( int recipient = PARENT_ID; recipient <= nProc; recipient++ ) {
-		if ( recipient == localId ) continue;
-		write( pipes[ localId ][ recipient ][ 1 ], msg, sizeof( Message ) );
-		// printf( "Send from %d to %d: %d\n", localId, recipient, (*msg).s_header.s_type );
+
+void makePipeLog() {
+
+	char buf[ 10 ];
+
+	for ( int row = 0; row <= nProc; row++ ) {
+
+		for ( int col = 0; col <= nProc; col++ ) {
+			sprintf( buf, "| %3d %3d ", pipes[ row ][ col ][ 0 ], pipes[ row ][ col ][ 1 ] );
+			write( pipesLog, buf, strlen( buf ) );
+		}
+
+		write( pipesLog, "|\n", 2 );
 	}
-	return 0;
 }
 
 
-int receive_any( void * self, Message * msg ) {
-	for ( int sender = PARENT_ID; sender <= nProc; sender++ ) {
-		// printf( "I am %d and I'm trying to receive message from %d... \n", localId, sender );
-		if ( sender == localId ) continue;
-		ssize_t wasRead = read( pipes[ sender ][ localId ][ 0 ], msg, sizeof( Message ) );
-		// printf( "Receive %d by %d\n", wasRead, localId );
-		if ( wasRead > 0 ) printf( "%d Receive message: %s", localId, (*msg).s_payload );
-		if ( wasRead > 0 ) break;
-		else if ( sender == nProc ) sender = PARENT_ID - 1;
-		// sleep(1);
+int send( void * self, local_id dst, const Message * msg) {
+
+	ssize_t wasWrite = write( pipes[ localId ][ dst ][ 1 ], msg, sizeof( Message ) );
+
+	// printf( "Send %d bytes from %d to %d\n", wasWrite, localId, dst );
+
+	return ( wasWrite > 0 ) ? IPC_SUCCESS : IPC_FAILURE;
+}
+
+
+
+int send_multicast( void * self, const Message * msg ) {
+
+	int status = IPC_SUCCESS;
+
+	for ( local_id dst = PARENT_ID; dst <= nProc; dst++ ) {
+		if ( dst == localId ) continue;
+		status = send( NULL, dst, msg );
+		if ( status == IPC_FAILURE ) break;
 	}
-	return 0;
+
+	return status;
+}
+
+
+
+int receive( void * self, local_id from, Message * msg ) {
+
+	ssize_t wasRead = read( pipes[ from ][ localId ][ 0 ], msg, sizeof( Message ) );
+
+	// if( wasRead > 0 ) printf( "Receive %d bytes by %d proc from %d proc\n", wasRead, localId, from );
+
+	return ( wasRead > 0 ) ? IPC_SUCCESS : IPC_FAILURE;
+}
+
+
+
+int receive_any( void * self, Message * msg ) {
+
+	static local_id sender = PARENT_ID + 1;
+
+	if( sender == localId ) sender++;
+
+	// printf( "receive_any by %d proc from %d proc\n", localId, sender );
+
+	return receive( NULL, sender++, msg );
 }
